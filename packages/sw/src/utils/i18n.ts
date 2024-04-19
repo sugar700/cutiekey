5e7ca6fa4de774db891df40e2f12fb883058a501
Luna Nova <her@mint.lgbt>
2024-04-19 08:00:53 -0400

    feat: redo `i18n.ts`

--- packages/sw/src/utils/i18n.ts
+++ packages/sw/src/utils/i18n.ts
@@ -1,37 +1,36 @@
-/*
- * SPDX-FileCopyrightText: syuilo and misskey-project
- * SPDX-License-Identifier: AGPL-3.0-only
- */
-
-export type Locale = { [key: string]: string | Locale };
+export type Locale = {
+  [key: string]: Locale | string
+}
 
 export class I18n<T extends Locale = Locale> {
-	public ts: T;
+  public ts: T
 
-	constructor(locale: T) {
-		this.ts = locale;
+  constructor(locale: T) {
+    this.t = this.t.bind(this)
 
-		//#region BIND
-		this.t = this.t.bind(this);
-		//#endregion
-	}
+    this.ts = locale
+  }
 
-	// string にしているのは、ドット区切りでのパス指定を許可するため
-	// なるべくこのメソッド使うよりもlocale直接参照の方がvueのキャッシュ効いてパフォーマンスが良いかも
-	public t(key: string, args?: Record<string, string>): string {
-		try {
-			let str = key.split('.').reduce<Locale | Locale[keyof Locale]>((o, i) => o[i], this.ts);
-			if (typeof str !== 'string') throw new Error();
+  public t(key: string, args?: Record<string, string>) {
+    try {
+      let str = key.split('.')
+        .reduce<Locale | Locale[keyof Locale]>((o, i) => (o as any)[i], this.ts)
 
-			if (args) {
-				for (const [k, v] of Object.entries(args)) {
-					str = str.replace(`{${k}}`, v);
-				}
-			}
-			return str;
-		} catch (err) {
-			console.warn(`missing localization '${key}'`);
-			return key;
-		}
-	}
+      if (typeof str !== 'string') {
+        throw new Error()
+      }
+
+      if (args) {
+        for (const [k, v] of Object.entries(args)) {
+          str = str.replace(`{${k}}`, v)
+        }
+      }
+
+      return str
+    } catch (err) {
+      console.warn(`[SW] Missing translation for \`${key}\`. Using key name.`)
+
+      return key
+    }
+  }
 }

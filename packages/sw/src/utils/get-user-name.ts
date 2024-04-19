a672d0567dab31a7a969ddce008652bb7fc35080
Luna Nova <her@mint.lgbt>
2024-04-19 08:00:27 -0400

    feat: redo `get-user-name.ts`

--- packages/sw/src/utils/get-user-name.ts
+++ packages/sw/src/utils/get-user-name.ts
@@ -1,8 +1,5 @@
-/*
- * SPDX-FileCopyrightText: syuilo and misskey-project
- * SPDX-License-Identifier: AGPL-3.0-only
- */
-
-export function getUserName(user: { name?: string | null; username: string }): string {
-	return user.name === '' ? user.username : user.name ?? user.username;
+export function getUserName(user: { name?: string | null; username: string }) {
+  return user.name === ''
+    ? user.username
+    : user.name ?? user.username
 }

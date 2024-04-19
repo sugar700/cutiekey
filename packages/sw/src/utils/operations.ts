40324a5795e37ea35d5d0559f87177212cd92320
Luna Nova <her@mint.lgbt>
2024-04-19 08:01:40 -0400

    feat: redo `operations.ts`

--- packages/sw/src/utils/operations.ts
+++ packages/sw/src/utils/operations.ts
@@ -1,84 +1,120 @@
-/*
- * SPDX-FileCopyrightText: syuilo and misskey-project
- * SPDX-License-Identifier: AGPL-3.0-only
- */
+import { getUrlWithLoginId } from '@/utils/get-url-with-login-id'
+import { getAccountFromId } from '@/utils/get-account-from-id'
+import type { SwMessage, SwMessageOrderType } from '@/types'
+import * as Misskey from 'misskey-js'
 
-/*
- * Operations
- * 各種操作
- */
-import * as Misskey from 'misskey-js';
-import type { SwMessage, SwMessageOrderType } from '@/types.js';
-import { getAccountFromId } from '@/utils/get-account-from-id.js';
-import { getUrlWithLoginId } from '@/utils/login-id.js';
+// Limit sending requests to `notifications/mark-all-as-read`
+const readBlockingStatus = new Map<string, boolean>()
 
-export const cli = new Misskey.api.APIClient({ origin, fetch: (...args): Promise<Response> => fetch(...args) });
+export const cli = new Misskey.api.APIClient({ origin })
 
-export async function api<E extends keyof Misskey.Endpoints, O extends Misskey.Endpoints[E]['req']>(endpoint: E, userId?: string, options?: O): Promise<void | ReturnType<typeof cli.request<E, O>>> {
-	let account: { token: string; id: string } | void = undefined;
+export async function api<
+  E extends keyof Misskey.Endpoints,
+  O extends Misskey.Endpoints[E]['req']
+>(
+  endpoint: E,
+  userId?: string,
+  options?: O
+  // @ts-expect-error
+): Promise<ReturnType<typeof cli.request<E, O>> | void> {
+  let account: { id: string; token: string } | void
 
-	if (userId) {
-		account = await getAccountFromId(userId);
-		if (!account) return;
-	}
+  if (userId) {
+    account = await getAccountFromId(userId)
 
-	return cli.request(endpoint, options, account?.token);
+    if (!account) {
+      return
+    }
+  }
+
+  // @ts-expect-error
+  return cli.request(endpoint, options, account?.token)
 }
 
-// mark-all-as-read送出を1秒間隔に制限する
-const readBlockingStatus = new Map<string, boolean>();
-export function sendMarkAllAsRead(userId: string): Promise<null | undefined | void> {
-	if (readBlockingStatus.get(userId)) return Promise.resolve();
-	readBlockingStatus.set(userId, true);
-	return new Promise(resolve => {
-		setTimeout(() => {
-			readBlockingStatus.set(userId, false);
-			api('notifications/mark-all-as-read', userId).then(resolve, resolve);
-		}, 1000);
-	});
+export async function findClient() {
+  const clients = await globalThis.clients.matchAll({ type: 'window' })
+
+  return clients.find(c => !new URL(c.url).searchParams.has('zen')) ?? null
 }
 
-// rendered acctからユーザーを開く
-export function openUser(acct: string, loginId?: string): ReturnType<typeof openClient> {
-	return openClient('push', `/@${acct}`, loginId, { acct });
+export function openAntenna(antennaId: string, loginId: string) {
+  return openClient('push', `/timeline/antenna/${antennaId}`, loginId, {
+    antennaId
+  })
 }
 
-// noteIdからノートを開く
-export function openNote(noteId: string, loginId?: string): ReturnType<typeof openClient> {
-	return openClient('push', `/notes/${noteId}`, loginId, { noteId });
+export async function openClient(
+  order: SwMessageOrderType,
+  url: string,
+  loginId?: string,
+  query: Record<string, SwMessage[string]> = {}
+) {
+  const client = await findClient()
+
+  if (client) {
+    client.postMessage({
+      ...query,
+      loginId,
+      order,
+      type: 'order',
+      url
+    } satisfies SwMessage)
+
+    return client
+  }
+
+  return globalThis.clients.openWindow(
+    loginId ? getUrlWithLoginId(url, loginId) : url
+  )
 }
 
-// noteIdからノートを開く
-export function openAntenna(antennaId: string, loginId: string): ReturnType<typeof openClient> {
-	return openClient('push', `/timeline/antenna/${antennaId}`, loginId, { antennaId });
+export function openNote(noteId: string, loginId?: string) {
+  return openClient('push', `/notes/${noteId}`, loginId, { noteId })
 }
 
-// post-formのオプションから投稿フォームを開く
-export async function openPost(options: { initialText?: string; reply?: Misskey.entities.Note; renote?: Misskey.entities.Note }, loginId?: string): ReturnType<typeof openClient> {
-	// クエリを作成しておく
-	const url = '/share';
-	const query = new URLSearchParams();
-	if (options.initialText) query.set('text', options.initialText);
-	if (options.reply) query.set('replyId', options.reply.id);
-	if (options.renote) query.set('renoteId', options.renote.id);
+export async function openPost(
+  opts: {
+    initialText?: string
+    renote?: Misskey.entities.Note
+    reply?: Misskey.entities.Note
+  },
+  loginId?: string
+) {
+  const query = new URLSearchParams()
 
-	return openClient('post', `${url}?${query}`, loginId, { options });
+  if (opts.initialText) {
+    query.set('text', opts.initialText)
+  }
+
+  if (opts.renote) {
+    query.set('renoteId', opts.renote.id)
+  }
+
+  if (opts.reply) {
+    query.set('replyId', opts.reply.id)
+  }
+
+  return openClient('post', `/share?${query}`, loginId, { options: opts })
 }
 
-export async function openClient(order: SwMessageOrderType, url: string, loginId?: string, query: Record<string, SwMessage[string]> = {}): Promise<WindowClient | null> {
-	const client = await findClient();
-
-	if (client) {
-		client.postMessage({ type: 'order', ...query, order, loginId, url } satisfies SwMessage);
-		return client;
-	}
-
-	return globalThis.clients.openWindow(loginId ? getUrlWithLoginId(url, loginId) : url);
+export function openUser(acct: string, loginId?: string) {
+  return openClient('push', `/@${acct}`, loginId, { acct })
 }
 
-export async function findClient(): Promise<WindowClient | null> {
-	const clients = await globalThis.clients.matchAll({
-		type: 'window',
-	});
-	return clients.find(c => !(new URL(c.url)).searchParams.has('zen')) ?? null;
+export function sendMarkAllAsRead(
+  userId: string
+): Promise<void | null | undefined> {
+  if (readBlockingStatus.get(userId)) {
+    return Promise.resolve()
+  }
+
+  readBlockingStatus.set(userId, true)
+
+  return new Promise(res => {
+    setTimeout(() => {
+      readBlockingStatus.set(userId, false)
+
+      api('notifications/mark-all-as-read', userId).then(res as any, res)
+    }, 1000)
+  })
 }
